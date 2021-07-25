c1cc76c1af4d4b108964ac93f0f45aa7d4aa70d5
junoy <junoy_ouyang@hotmail.com>
2021-07-25 21:35:43 +0800

    Initialize social-login

---
+++ src/api/validations/auth.validation.js
@@ -0,0 +1,39 @@
+const Joi = require('joi');
+
+module.exports = {
+  // POST /v1/auth/register
+  register: {
+    body: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required().min(6).max(128),
+    },
+  },
+
+  // POST /v1/auth/login
+  login: {
+    body: {
+      email: Joi.string().email().required(),
+      password: Joi.string().required().max(128),
+    },
+  },
+
+  // TODO: test
+  // POST /v1/auth/facebook
+  // POST /v1/auth/google
+  oAuth: {
+    // header: {
+    //   Authorization: Joi.string().required(),
+    // },
+    // body: {
+    //   access_token: Joi.string().required(),
+    // }
+  },
+
+  // POST /v1/auth/refresh
+  refresh: {
+    body: {
+      email: Joi.string().email().required(),
+      refreshToken: Joi.string().required(),
+    },
+  },
+};
